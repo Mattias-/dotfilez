223d8ff417c973d04f30b126b4e38a3cf0df25b4
Mattias Appelgren <mattias@ppelgren.se>
2013-11-23 12:06:12 +0100

    Fixed install.sh bug

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 files=(.bashrc .bash_profile .gitconfig .screenrc .vimrc .vim)
 for file in "${files[@]}"
 do
-    ln -s  $(pwd)/$file ~/ 2> /dev/null 
+    ln -s  $(dirname $0)/$file ~/ 2> /dev/null 
     if [ $? -ne 0 ]; then
         mv ~/$file ~/$file.bak$(date +%F)
         echo "Renamed old $file to $file.bak$(date +%F)"
-        ln -s  $(pwd)/$file ~/ 2> /dev/null 
+        ln -s  $(dirname $0)/$file ~/ 2> /dev/null 
     fi
 done
