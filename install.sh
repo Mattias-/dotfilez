ac35ae5a0d09ff0206a9cd3deefb6e063b023fa1
Mattias Appelgren <mattias@ppelgren.se>
2014-04-22 01:52:33 -0700

    Changes to install and setup

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-files=(.bashrc .bash_profile .gitconfig .screenrc .vimrc .vim)
+files=(.bashrc .bash_profile .gitconfig .screenrc .vimrc .vim bin .gitignore_global)
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 for file in "${files[@]}"
 do
-    ln -s  $(dirname $0)/$file ~/ 2> /dev/null 
+    ln -s  $SCRIPTPATH/$file ~/ 2> /dev/null 
     if [ $? -ne 0 ]; then
         mv ~/$file ~/$file.bak$(date +%F)
         echo "Renamed old $file to $file.bak$(date +%F)"
-        ln -s  $(dirname $0)/$file ~/ 2> /dev/null 
+        ln -s  $SCRIPTPATH/$file ~/ 2> /dev/null 
     fi
 done
