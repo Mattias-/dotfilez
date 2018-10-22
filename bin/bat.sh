2e44cc8ee390a34e184c70185afa206601bace5b
Mattias Appelgren <mattias@ppelgren.se>
2018-10-22 18:13:30 +0200

    fmt shell script

--- bin/bat.sh
+++ bin/bat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 if [ "$(uname)" == "Darwin" ]; then
-	pmset -g batt | tail -1 | cut -f2 | cut -d ';' -f1
+    pmset -g batt | tail -1 | cut -f2 | cut -d ';' -f1
 else
     upower -i "$(upower -e | grep BAT)" | grep percentage | awk '{ print $2 }'
 fi
