764481ac8698373e7eb2fe3116ff8f11ef22ff81
Mattias Appelgren <mattias@ppelgren.se>
2018-11-03 16:53:33 +0100

    Format python script

--- setup_dotfiles.py
+++ setup_dotfiles.py
@@ -5,29 +5,29 @@ import os
 import shutil
 
 FILES = [
-    '.bashrc',
-    '.bash_profile',
-    '.functions',
-    '.gitconfig',
-    '.gitignore_global',
-    '.gitattributes_global',
-    '.vim',
-    '.screenrc',
-    '.ssh/*',
-    '.config/*',
-    '.fonts',
-    '.tmux.conf',
-    'bin/*',
+    ".bashrc",
+    ".bash_profile",
+    ".functions",
+    ".gitconfig",
+    ".gitignore_global",
+    ".gitattributes_global",
+    ".vim",
+    ".screenrc",
+    ".ssh/*",
+    ".config/*",
+    ".fonts",
+    ".tmux.conf",
+    "bin/*",
 ]
 
 
 def main():
     base_dir = os.path.dirname(os.path.realpath(__file__))
-    home_dir = os.path.expanduser('~')
+    home_dir = os.path.expanduser("~")
 
     files = []
     for name in FILES:
-        if name.endswith('/*'):
+        if name.endswith("/*"):
             dir_name = os.path.dirname(name)
             for file_name in os.listdir(os.path.join(base_dir, dir_name)):
                 files.append(os.path.join(dir_name, file_name))
@@ -50,10 +50,10 @@ def link(source_file, target_file):
         if os.path.islink(target_file):
             os.unlink(target_file)
         else:
-            bak = target_file + '.bak.' + datetime.datetime.now().isoformat()
+            bak = target_file + ".bak." + datetime.datetime.now().isoformat()
             shutil.move(target_file, bak)
     os.symlink(os.path.relpath(source_file, start=target_dir), target_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
