d49be0f1f4cb6c7bd39317b0b1e4eea5bea9ab80
Mattias Appelgren <mattias@ppelgren.se>
2019-03-16 16:16:37 +0100

    Update battery status script

--- bin/bat.sh
+++ bin/bat.sh
@@ -3,5 +3,9 @@ set -euo pipefail
 if [ "$(uname)" == "Darwin" ]; then
     pmset -g batt | tail -1 | cut -f2 | cut -d ';' -f1
 else
-    upower -i "$(upower -e | grep BAT)" | grep percentage | awk '{ print $2 }'
+    if hash upower; then
+        upower -i "$(upower -e | grep BAT)" | grep percentage | awk '{ print $2 }'
+    else
+        grep "POWER_SUPPLY_CAPACITY=" /sys/class/power_supply/BAT0/uevent | cut -d"=" -f2
+    fi
 fi
