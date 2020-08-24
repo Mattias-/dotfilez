649d49ec0be3eabe96cc1053cc8a387a8fb21e61
Mattias Appelgren <mattias.appelgren@tink.se>
2020-08-24 22:35:48 +0200

    hyper: Add plugins

--- .hyper.js
+++ .hyper.js
@@ -146,7 +146,11 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hypersixteen"],
+  plugins: [
+    "hypersixteen",
+    "hyperminimal",
+    "hyperborder",
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
