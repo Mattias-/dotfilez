41840baaac16a2724408fb56142666ea289e2d38
Mattias Appelgren <mattias@ppelgren.se>
2021-02-13 17:52:40 +0100

    waybar: Fix now_playing

--- bin/now_playing.sh
+++ bin/now_playing.sh
@@ -3,10 +3,11 @@ if [ "$(uname)" == "Darwin" ]; then
     SPOTIFY_ARTIST=$(osascript -e 'tell application "Spotify" to artist of current track as string')
     SPOTIFY_TITLE=$(osascript -e 'tell application "Spotify" to name of current track as string')
 else
-    eval "$(sp eval)" &>/dev/null
+    if pgrep spotify &>/dev/null; then
+        eval "$(sp eval "$@")" &>/dev/null
+    fi
 fi
 
-if [ "$SPOTIFY_ARTIST" != "" ]; then
-    echo " $SPOTIFY_TITLE • $SPOTIFY_ARTIST"
-    #echo "♫ $SPOTIFY_TITLE - $SPOTIFY_ARTIST"
+if [ "$SPOTIFY_ARTIST" != "" ] || [ "$SPOTIFY_SHOW_NAME" != "" ]; then
+    echo " ${SPOTIFY_TITLE:0:40} • ${SPOTIFY_ARTIST:0:40}${SPOTIFY_SHOW_NAME:0:40}"
 fi
