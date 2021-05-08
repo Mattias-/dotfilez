05703faef691c4b2ef0f049534f76dc8b07258e2
Mattias Appelgren <mattias@ppelgren.se>
2021-05-08 16:24:39 +0200

    Use editorconfig

--- nvim/ftplugin/javascript.vim
+++
@@ -1,4 +0,0 @@
-
-set tabstop=2
-set shiftwidth=2 "When indenting use x spaces
-set softtabstop=2   " makes the spaces feel like real tabs
--- setup_dotfiles.py
+++ setup_dotfiles.py
@@ -7,6 +7,7 @@ import shutil
 FILES = [
     ".bashrc",
     ".bash_profile",
+    ".editorconfig",
     ".functions",
     ".aliases",
     ".gitconfig",
