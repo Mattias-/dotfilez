d4cfa202986af8172a398c9e24427cc8986ae2d9
Mattias Appelgren <mattias@ppelgren.se>
2021-07-18 11:30:34 +0200

    Update backup script

--- backup.sh
+++ backup.sh
@@ -4,6 +4,14 @@ set -euo pipefail
 dry_run=false
 encrypt=true
 
+restore_paths=(
+    .ssh
+    kp
+    work
+    projects
+    .config/rclone
+)
+
 backup_paths=(
     background.png
     .config
@@ -23,29 +31,42 @@ exclude_paths=(
     .config/google-chrome
 )
 
-date=$(date -u "+%Y-%m-%d_%H-%M")
-dest="./backup-${date}"
-rsync_args=()
+backup() {
+    date=$(date -u "+%Y-%m-%d_%H-%M")
+    dest="./backup-${date}"
+    rsync_args=()
 
-if $dry_run; then
-    rsync_args+=("--dry-run")
-fi
+    if $dry_run; then
+        rsync_args+=("--dry-run")
+    fi
 
-for ep in ${exclude_paths[*]}; do
-    rsync_args+=("--exclude=$ep")
-done
+    for ep in ${exclude_paths[*]}; do
+        rsync_args+=("--exclude=$ep")
+    done
 
-rsync -av \
-    "${rsync_args[@]}" \
-    "${backup_paths[@]}" \
-    "$dest"
+    rsync -av \
+        "${rsync_args[@]}" \
+        "${backup_paths[@]}" \
+        "$dest"
 
-find "$dest" -type f -size +50M
-tar -czf "$dest.tar.gz" "$dest"
-if $encrypt; then
-    gpg --output "$dest.tar.gz.gpg" --symmetric "$dest.tar.gz"
-fi
+    find "$dest" -type f -size +50M
+    tar -czf "$dest.tar.gz" "$dest"
+    if $encrypt; then
+        gpg --output "$dest.tar.gz.gpg" --symmetric "$dest.tar.gz"
+    fi
+}
 
 restore() {
-    gpg --decrypt "$1" | tar -xz
+    #gpg --decrypt "$1.tar.gz.gpg" | tar -xz
+    for rp in ${restore_paths[*]}; do
+        echo "cp -ri $1/$rp ~"
+    done
+    #cp -ri $1/.ssh ~
+    #cp -ri $1/.aws ~
+    #cp -ri $1/kp ~
+    #cp -ri $1/work ~
+    #cp -ri $1/projects ~
+    #cp -ri $1/.config/rclone ./.config/
 }
+
+restore "$1"
