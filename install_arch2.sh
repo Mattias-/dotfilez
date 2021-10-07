ce6244a2662786c3ca9871dbf251c161eba260f3
Mattias Appelgren <mattias@ppelgren.se>
2021-10-07 21:31:20 +0200

    install_arch: patch boot entries

--- install_arch2.sh
+++ install_arch2.sh
@@ -22,6 +22,18 @@ main() {
 
     ln -sf /run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
     rm -f /mnt/etc/systemd/network/20-ethernet.network
+
+    patch_boot_entries
+}
+
+patch_boot_entries() {
+    find /boot/loader/entries/ -type f | while read -r entry_file; do
+        cat "$entry_file"
+        new=$(sed -r 's/options (.*)/options \1 quiet/g' "$entry_file")
+        diff --color "$entry_file" <(echo "$new")
+        #echo "$new" >"$entry_file"
+        #cat "$entry_file"
+    done
 }
 
 main
