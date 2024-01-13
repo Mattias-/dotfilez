97deb17943b735908b693c0f9522b6029d4ec91b
Mattias Appelgren <mattias@ppelgren.se>
2024-01-13 20:35:24 +0100

    nvim: Add copilot

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -34,4 +34,14 @@ return {
             { "<leader>g", "<cmd>Git<cr>", desc = "Toggle Git" },
         },
     },
+    {
+        "github/copilot.vim",
+        config = function()
+            vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+                expr = true,
+                replace_keycodes = false
+            })
+            vim.g.copilot_no_tab_map = true
+        end,
+    },
 }
