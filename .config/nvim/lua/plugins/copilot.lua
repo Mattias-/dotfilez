08ba028ecc5ef65e3fd54ebb1e259179f943205c
Mattias Appelgren <mattias@ppelgren.se>
2025-11-29 10:08:34 +0100

    Add copilot nes

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -13,7 +13,11 @@ return {
     },
     {
         "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
         event = "InsertEnter",
+        requires = {
+            "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
+        },
         opts = {
             should_attach = function(_, bufname)
                 if string.match(bufname, "Downloads") then
