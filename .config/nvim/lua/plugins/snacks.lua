2439766b66d551b692e287b5395f31fea13e6cda
Mattias Appelgren <mattias@ppelgren.se>
2025-12-18 21:27:46 +0100

    nvim: Move some telescope keybinds to snacks picker

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -41,15 +41,16 @@ return {
             -- Top Pickers & Explorer
             { "<leader><space>", function() Snacks.picker.smart() end,                                   desc = "Smart Find Files" },
             { "<leader>,",       function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
-            { "<leader>/",       function() Snacks.picker.grep() end,                                    desc = "Grep" },
             { "<leader>:",       function() Snacks.picker.command_history() end,                         desc = "Command History" },
             { "<leader>n",       function() Snacks.picker.notifications() end,                           desc = "Notification History" },
             { "<leader>e",       function() Snacks.explorer() end,                                       desc = "File Explorer" },
             -- find
             { "<leader>fb",      function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
+            { "<leader>b",       function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
             { "<leader>fc",      function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
             { "<leader>ff",      function() Snacks.picker.files() end,                                   desc = "Find Files" },
             { "<leader>fg",      function() Snacks.picker.git_files() end,                               desc = "Find Git Files" },
+            { "<C-p>",           function() Snacks.picker.git_files() end,                               desc = "Find Git Files" },
             { "<leader>fp",      function() Snacks.picker.projects() end,                                desc = "Projects" },
             { "<leader>fr",      function() Snacks.picker.recent() end,                                  desc = "Recent" },
             -- git
@@ -68,7 +69,10 @@ return {
             -- Grep
             { "<leader>sb",      function() Snacks.picker.lines() end,                                   desc = "Buffer Lines" },
             { "<leader>sB",      function() Snacks.picker.grep_buffers() end,                            desc = "Grep Open Buffers" },
+            { "<leader>/",       function() Snacks.picker.grep() end,                                    desc = "Grep" },
+            { "<C-g>",           function() Snacks.picker.grep() end,                                    desc = "Grep" },
             { "<leader>sg",      function() Snacks.picker.grep() end,                                    desc = "Grep" },
+            { "<leader>pg",      function() Snacks.picker.grep() end,                                    desc = "Grep" },
             { "<leader>sw",      function() Snacks.picker.grep_word() end,                               desc = "Visual selection or word",      mode = { "n", "x" } },
             -- search
             { '<leader>s"',      function() Snacks.picker.registers() end,                               desc = "Registers" },
@@ -84,6 +88,7 @@ return {
             { "<leader>si",      function() Snacks.picker.icons() end,                                   desc = "Icons" },
             { "<leader>sj",      function() Snacks.picker.jumps() end,                                   desc = "Jumps" },
             { "<leader>sk",      function() Snacks.picker.keymaps() end,                                 desc = "Keymaps" },
+            { "<leader>pk",      function() Snacks.picker.keymaps() end,                                 desc = "Keymaps" },
             { "<leader>sl",      function() Snacks.picker.loclist() end,                                 desc = "Location List" },
             { "<leader>sm",      function() Snacks.picker.marks() end,                                   desc = "Marks" },
             { "<leader>sM",      function() Snacks.picker.man() end,                                     desc = "Man Pages" },
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -30,31 +30,6 @@ return {
                 "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.expand('<cWORD>') })<cr>",
                 desc = "Find WORD"
             },
-            {
-                "<C-p>",
-                "<cmd>lua require('telescope.builtin').git_files()<cr>",
-                desc = "Find git files"
-            },
-            {
-                "<C-g>",
-                "<cmd>lua require('telescope.builtin').live_grep()<cr>",
-                desc = "Live grep"
-            },
-            {
-                "<leader>pg",
-                "<cmd>lua require('telescope.builtin').live_grep()<cr>",
-                desc = "Live grep"
-            },
-            {
-                "<leader>pk",
-                "<cmd>lua require('telescope.builtin').keymaps()<cr>",
-                desc = "Find keymaps"
-            },
-            {
-                "<leader>b",
-                "<cmd>lua require('telescope.builtin').buffers()<cr>",
-                desc = "Find buffers"
-            },
         },
     }
 }
